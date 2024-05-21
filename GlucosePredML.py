4d30942ead0694479f3b6135707399f3668b5be6
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-05-21 17:09:53 -0400

    Add files via upload

--- GlucosePredML.py
+++ GlucosePredML.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from neuralprophet import NeuralProphet
-import statistics
 
-GlucoseTable = pd.read_csv(r'C:\Users\loren\Downloads\glucosevalues1.csv')
+GlucoseTable = pd.read_csv('glucosevalues1.csv')
 
 GlucoseTable.pop('Index')
 GlucoseTable.pop('Transmitter ID')
@@ -27,6 +26,7 @@ GlucoseTable['ds'] = GlucoseTable['ds'].str[:10] + ' ' + GlucoseTable['ds'].str[
 GlucoseTable['y'] = GlucoseTable['y'].astype('string')
 GlucoseTable['y'] = GlucoseTable['y'].replace('Low', float('nan'))
 GlucoseTable['y'] = GlucoseTable['y'].replace('0', float('nan'))
+GlucoseTable['y'] = GlucoseTable['y'].replace('High', '')
 
 GlucoseTable = GlucoseTable.dropna()
 
@@ -34,39 +34,32 @@ GlucoseTable['y'] = GlucoseTable['y'].astype('float')
 GlucoseTable['ds'] = GlucoseTable['ds'].astype('datetime64[ns]')
 
 model = NeuralProphet(
-    learning_rate = 0.1
+    learning_rate = 0.1,
+    growth = 'linear',
+    epochs = 130,
+    changepoints_range = 20
 )
+
 GlucoseValTest = model.fit(GlucoseTable)
 
-Future = model.make_future_dataframe(GlucoseTable, periods = 300)
+Future = model.make_future_dataframe(GlucoseTable, periods = 2000)
 
 FutureVals = model.predict(Future)
 
-Model_OriginalVals = model.predict(GlucoseTable)
+Model_OriginalVals_Pred = model.predict(GlucoseTable)
 
 plt.title('Glucose Values (mg/dL) vs. Days')
 plt.xlabel('Days')
 plt.ylabel('Glucose Values (mg/dL)')
 plt.plot(GlucoseTable['ds'], GlucoseTable['y'], label = 'Original Glucose Values', c ='g')
 plt.plot(FutureVals['ds'], FutureVals['yhat1'], label = 'Future Glucose Values', c = 'r')
-plt.plot(Model_OriginalVals['ds'], Model_OriginalVals['yhat1'], label = 'ML Model Predicted Glucose Values', c = 'b')
-plt.legend(loc = 'lower right')
+plt.plot(Model_OriginalVals_Pred['ds'], Model_OriginalVals_Pred['yhat1'], label = 'ML Model Predicted Glucose Values', c = 'b')
+plt.legend(loc = 'upper center')
 
 window = plt.get_current_fig_manager()
 window.full_screen_toggle()
 
 plt.show()
-
-
-Avg_Val_ML_Model = statistics.mean(Model_OriginalVals['yhat1'])
-print(Avg_Val_ML_Model)
-
-Avg_Val_OG_Model = statistics.mean(GlucoseTable['y'])
-print(Avg_Val_OG_Model)
-
-percent_error = ((Avg_Val_OG_Model - Avg_Val_ML_Model)/Avg_Val_OG_Model) * 100
-
-print(round(percent_error))
 
 
 
