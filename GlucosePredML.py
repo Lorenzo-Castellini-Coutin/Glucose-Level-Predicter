08058c21132d37b70182aa4de8c5c7ea4ca10e68
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-05-21 13:19:35 -0400

    Add files via upload

--- GlucosePredML.py
+++ GlucosePredML.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from neuralprophet import NeuralProphet
+import statistics
 
-GlucoseTable = pd.read_csv('glucosevalues.csv')
+GlucoseTable = pd.read_csv(r'C:\Users\loren\Downloads\glucosevalues1.csv')
 
 GlucoseTable.pop('Index')
 GlucoseTable.pop('Transmitter ID')
@@ -17,16 +18,24 @@ GlucoseTable.pop('Source Device ID')
 GlucoseTable.pop('Event Subtype')
 GlucoseTable.pop('Insulin Value (u)')
 
-GlucoseTable['Timestamp (YYYY-MM-DDThh:mm:ss)'] = GlucoseTable['Timestamp (YYYY-MM-DDThh:mm:ss)'].astype('string')
-GlucoseTable['Timestamp (YYYY-MM-DDThh:mm:ss)'] = GlucoseTable['Timestamp (YYYY-MM-DDThh:mm:ss)'].str.replace('T', '')
-GlucoseTable['Timestamp (YYYY-MM-DDThh:mm:ss)'] = GlucoseTable['Timestamp (YYYY-MM-DDThh:mm:ss)'].str[:10] + ' ' + GlucoseTable['Timestamp (YYYY-MM-DDThh:mm:ss)'].str[10:]
+GlucoseTable.columns = ['ds', 'y']    #ds is dates and y is glucose values
+
+GlucoseTable['ds'] = GlucoseTable['ds'].astype('string')
+GlucoseTable['ds'] = GlucoseTable['ds'].str.replace('T', '')
+GlucoseTable['ds'] = GlucoseTable['ds'].str[:10] + ' ' + GlucoseTable['ds'].str[10:]
+
+GlucoseTable['y'] = GlucoseTable['y'].astype('string')
+GlucoseTable['y'] = GlucoseTable['y'].replace('Low', float('nan'))
+GlucoseTable['y'] = GlucoseTable['y'].replace('0', float('nan'))
 
 GlucoseTable = GlucoseTable.dropna()
 
-GlucoseTable.columns = ['ds', 'y']
+GlucoseTable['y'] = GlucoseTable['y'].astype('float')
 GlucoseTable['ds'] = GlucoseTable['ds'].astype('datetime64[ns]')
 
-model = NeuralProphet()
+model = NeuralProphet(
+    learning_rate = 0.1
+)
 GlucoseValTest = model.fit(GlucoseTable)
 
 Future = model.make_future_dataframe(GlucoseTable, periods = 300)
@@ -47,4 +56,19 @@ window = plt.get_current_fig_manager()
 window.full_screen_toggle()
 
 plt.show()
+
+
+Avg_Val_ML_Model = statistics.mean(Model_OriginalVals['yhat1'])
+print(Avg_Val_ML_Model)
+
+Avg_Val_OG_Model = statistics.mean(GlucoseTable['y'])
+print(Avg_Val_OG_Model)
+
+percent_error = ((Avg_Val_OG_Model - Avg_Val_ML_Model)/Avg_Val_OG_Model) * 100
+
+print(round(percent_error))
+
+
+
+
 
