19ce66ea0de2e83b4033949094e0e65a601f4906
Lorenzo Castellini Coutin <lorenzo.castellini28@gmail.com>
2024-07-17 12:45:52 -0400

    New ML model

--- GlucosePred.py
+++ GlucosePred.py
@@ -1,73 +1,85 @@
 #Author: Lorenzo Castellini Coutin
 
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from neuralprophet import NeuralProphet
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+import xgboost as xgb
 
-GlucoseTable = pd.read_csv()   #Insert the csv file here
+GlucoseTable = pd.read_csv()   
 
-GlucoseTable.pop('Index')
-GlucoseTable.pop('Transmitter ID')
-GlucoseTable.pop('Transmitter Time (Long Integer)')
-GlucoseTable.pop('Glucose Rate of Change (mg/dL/min)')
-GlucoseTable.pop('Carb Value (grams)')
-GlucoseTable.pop('Event Type')
-GlucoseTable.pop('Patient Info')
-GlucoseTable.pop('Duration (hh:mm:ss)')
-GlucoseTable.pop('Device Info')
-GlucoseTable.pop('Source Device ID')
-GlucoseTable.pop('Event Subtype')
-GlucoseTable.pop('Insulin Value (u)')
+columns_delete = [
+    'Index', 'Transmitter ID', 'Transmitter Time (Long Integer)', 'Glucose Rate of Change (mg/dL/min)','Carb Value (grams)', 'Event Type',
+    'Patient Info', 'Duration (hh:mm:ss)', 'Device Info', 'Source Device ID', 'Event Subtype', 'Insulin Value (u)'
+]
 
-GlucoseTable.columns = ['ds', 'y']    #ds is dates and y is glucose values
+GlucoseTable = GlucoseTable.drop(columns = columns_delete)
 
-GlucoseTable['ds'] = GlucoseTable['ds'].astype('string')
-GlucoseTable['ds'] = GlucoseTable['ds'].str.replace('T', '')
-GlucoseTable['ds'] = GlucoseTable['ds'].str[:10] + ' ' + GlucoseTable['ds'].str[10:]
+GlucoseTable.columns = ['Dates', 'GlucoseLvls']    
 
-GlucoseTable['y'] = GlucoseTable['y'].astype('string')
-GlucoseTable['y'] = GlucoseTable['y'].replace('Low', float('nan'))
-GlucoseTable['y'] = GlucoseTable['y'].replace('0', float('nan'))
-GlucoseTable['y'] = GlucoseTable['y'].replace('High', float('nan'))
+GlucoseTable['Dates'] = GlucoseTable['Dates'].astype('string')
+GlucoseTable['Dates'] = GlucoseTable['Dates'].str.replace('T', '')
+GlucoseTable['Dates'] = GlucoseTable['Dates'].str[:10] + ' ' + GlucoseTable['Dates'].str[10:]
+
+GlucoseTable = GlucoseTable[GlucoseTable['GlucoseLvls'] != 0.0]
+GlucoseTable = GlucoseTable[GlucoseTable['GlucoseLvls'] != 'Low']
+GlucoseTable = GlucoseTable[GlucoseTable['GlucoseLvls'] != 'High']
 
 GlucoseTable = GlucoseTable.dropna()
 
-GlucoseTable['y'] = GlucoseTable['y'].astype('float')
-GlucoseTable['ds'] = GlucoseTable['ds'].astype('datetime64[ns]')
+GlucoseTable['GlucoseLvls'] = GlucoseTable['GlucoseLvls'].astype('float')
+GlucoseTable['Dates'] = GlucoseTable['Dates'].astype('datetime64[ns]')
 
-model = NeuralProphet(
+def lag_features(df, lags): 
+    df_lagged = df.copy()
+    for lag in lags:
+        df_lagged[f'lag_{lag}'] = df_lagged['GlucoseLvls'].shift(lag)
+    return df_lagged
+
+lags_num = [1, 2, 3, 4, 5, 6, 7]  
+GlucoseTable = lag_features(GlucoseTable, lags_num)
+GlucoseTable = GlucoseTable.dropna()
+
+# Split data into features and target
+X = GlucoseTable[['lag_' + str(lag) for lag in lags_num]]
+Y = GlucoseTable['GlucoseLvls']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.1, shuffle = False)
+
+
+model = xgb.XGBRegressor(
+    objective='reg:squarederror', 
+    eval_metric='mae',
+    booster = 'dart',
     learning_rate = 0.1,
-    growth = 'linear',
-    epochs = 130,
-    changepoints_range = 20
+    max_depth = 4,
+    min_child_weight = 4, 
+    subsample = 0.7,
+    n_estimators = 70
 )
+model.fit(X_train, y_train)
 
-GlucoseValTest = model.fit(GlucoseTable)
 
-Future = model.make_future_dataframe(GlucoseTable, periods = 2000)
+y_pred = model.predict(X_test)
 
-FutureVals = model.predict(Future)
 
-Model_OriginalVals_Pred = model.predict(GlucoseTable)
+mae = mean_absolute_error(y_test, y_pred)
+print(f'Mean Absolute Error: {mae}')
 
-plt.title('Glucose Values (mg/dL) vs. Days')
-plt.xlabel('Days')
+
+plt.plot(GlucoseTable['Dates'].iloc[-len(y_pred):], y_pred, label='Predicted Values', color='red')
+plt.plot(GlucoseTable['Dates'], GlucoseTable['GlucoseLvls'], label = '', color = 'blue')
+plt.xlabel('Dates (Year-Month-Day)')
 plt.ylabel('Glucose Values (mg/dL)')
-plt.plot(GlucoseTable['ds'], GlucoseTable['y'], label = 'Original Glucose Values', c ='g')
-plt.plot(FutureVals['ds'], FutureVals['yhat1'], label = 'Future Glucose Values', c = 'r')
-plt.plot(Model_OriginalVals_Pred['ds'], Model_OriginalVals_Pred['yhat1'], label = 'ML Model Predicted Glucose Values', c = 'b')
-plt.legend(loc = 'upper center')
-
+plt.title('Glucose Values Forecast')
+plt.legend()
+plt.grid(True)
 window = plt.get_current_fig_manager()
 window.full_screen_toggle()
-
 plt.show()
 
 
-#hello
-
-
-
-
-
+#Author: Lorenzo Castellini Coutin
 #Author: Lorenzo Castellini Coutin
